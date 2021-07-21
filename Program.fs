552369cb6ed985047f8a15aebf0a2c2984630f17
houstonhaynes <h3techsme@gmail.com>
2021-07-20 22:29:15 -0700

    Working with onboardLED

--- Program.fs
+++ Program.fs
@@ -10,61 +10,56 @@ open Meadow.Foundation.Sensors.Atmospheric
 type MeadowApp() =
     inherit App<F7Micro, MeadowApp>()
 
-    // set up sensor
-    let i2c = MeadowApp.Device.CreateI2cBus(Hardware.I2cBusSpeed.Standard)
-    let sensor = new Ccs811 (i2c)
-
-    // set up thresholds to compare against observer readings
-    let warningThreshold = Nullable (Units.Concentration(750.0, Units.Concentration.UnitType.PartsPerMillion))
-    let dangerThreshold = Nullable (Units.Concentration(1500.0, Units.Concentration.UnitType.PartsPerMillion))
-    
-    let mutable latestCO2Value = System.Nullable()
-
-    let consumer = Ccs811.CreateObserver(fun result -> 
-        let newValue = match result.New with | (new_val, _) -> new_val
-        latestCO2Value <- newValue
-        printfn $"New CO2 value: {latestCO2Value}")
-
-    let s = sensor.Subscribe(consumer)
-
-    do sensor.StartUpdating(TimeSpan.FromSeconds(2.0))
-
     // boilerplate LED stuff
     let led =
         RgbPwmLed(MeadowApp.Device, MeadowApp.Device.Pins.OnboardLedRed, MeadowApp.Device.Pins.OnboardLedGreen,
                       MeadowApp.Device.Pins.OnboardLedBlue, 3.3f, 3.3f, 3.3f,
                       Peripherals.Leds.IRgbLed.CommonType.CommonAnode)
+    do led.Stop |> ignore
 
+    // set up sensor
+    let i2c = MeadowApp.Device.CreateI2cBus(Hardware.I2cBusSpeed.Standard)
+    let sensor = new Ccs811 (i2c)
 
-    //let showColorBlinks color duration =
-    //    led.StartBlink(color, (duration / 2), (duration / 2), 0.75f, 0.0f) |> ignore
-    //    Threading.Thread.Sleep(int duration) |> ignore
-    //    led.Stop |> ignore
+    let warningThreshold = Nullable (Units.Concentration(750.0, Units.Concentration.UnitType.PartsPerMillion))
+    
+    let mutable latestCO2Value =  Nullable (Units.Concentration(750.0, Units.Concentration.UnitType.PartsPerMillion))
 
-    // set up relays
+    let mutable onboardLEDColor = 
+        match latestCO2Value.Value.PartsPerMillion with  
+        | i when i > 1500.0 -> Color.Red
+        | i when i > 750.0 && i <= 1500.0 -> Color.OrangeRed
+        | i when i > 650.0 && i <= 750.0 -> Color.Yellow
+        | _ -> Color.Green
+    
+    // set up ventilation
     let relayGreen = Relays.Relay(MeadowApp.Device, MeadowApp.Device.Pins.D05)
+    let mutable ventilationIsOn : bool = false
 
     let toggleRelays duration =
-        while latestCO2Value.Value.PartsPerMillion > warningThreshold.Value.PartsPerMillion do 
-            //Console.WriteLine $"latest CO2 Value: {latestCO2Value.Value.PartsPerMillion}, warning theshold {warningThreshold.Value.PartsPerMillion}"
-            relayGreen.Toggle()
-            Thread.Sleep(int (duration / 2))
-            relayGreen.Toggle()
-            Thread.Sleep(int (duration / 2))
+        async {
+            while latestCO2Value.Value.PartsPerMillion > warningThreshold.Value.PartsPerMillion do
+                ventilationIsOn <- true
+                led.SetColor(onboardLEDColor)
+                relayGreen.Toggle()
+                Thread.Sleep(int (duration))
+                relayGreen.Toggle()
+                Thread.Sleep(int (duration))
+            ventilationIsOn <- false
+            printfn $"Ventilator OFF..."
+        }
 
-    // TODO: put this on an event
-    do Thread.Sleep(5000)
-    do Console.WriteLine $"latest CO2 Value: {latestCO2Value.Value.PartsPerMillion}, warning theshold {warningThreshold.Value.PartsPerMillion}"
-    do toggleRelays 2000
-    do Thread.Sleep(10000)
-    do Console.WriteLine $"latest CO2 Value: {latestCO2Value.Value.PartsPerMillion}, warning theshold {warningThreshold.Value.PartsPerMillion}"
-    do toggleRelays 2000
+    do sensor.StartUpdating(TimeSpan.FromSeconds(2.0))
 
-    //let cycleColors  (firstColor: Color) duration =
-    //    while true do
-    //        showColorBlinks firstColor duration
+    let consumer = Ccs811.CreateObserver(fun result -> 
+        let newValue = match result.New with | (new_val, _) -> new_val
+        latestCO2Value <- newValue
+        printfn $"New CO2 value: {latestCO2Value}"
+        led.SetColor(onboardLEDColor, 100f)
+        if latestCO2Value.Value.PartsPerMillion > warningThreshold.Value.PartsPerMillion && ventilationIsOn = false then 
+            do toggleRelays 2000 |> Async.StartAsTask |> ignore)
 
-    //do cycleColors Color.Blue 2000
+    let s = sensor.Subscribe(consumer)
 
 [<EntryPoint>]
 let main argv =
