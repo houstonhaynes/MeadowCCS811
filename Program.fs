dbe25f2c508f81ae7ae94fcfebea15ed8bde6a94
houstonhaynes <h3techsme@gmail.com>
2021-07-24 04:29:44 -0700

    added filter for repeated CO2 values

--- Program.fs
+++ Program.fs
@@ -1,5 +1,4 @@
 open System
-open System.Threading
 open Meadow
 open Meadow.Devices
 open Meadow.Foundation
@@ -15,14 +14,13 @@ type MeadowApp() =
 
     let triggerThreshold = Nullable (Units.Concentration(750.0, Units.Concentration.UnitType.PartsPerMillion))
     let reductionThreshold = Nullable (Units.Concentration(650.0, Units.Concentration.UnitType.PartsPerMillion))
-    let mutable latestCO2Value =  Nullable (Units.Concentration(400.0, Units.Concentration.UnitType.PartsPerMillion))
+    let mutable latestCO2Value = Nullable (Units.Concentration(400.0, Units.Concentration.UnitType.PartsPerMillion))
+    let mutable previousCO2Value = Nullable (Units.Concentration(0.0, Units.Concentration.UnitType.PartsPerMillion))
 
     let mutable display = new Gc9a01 (MeadowApp.Device, MeadowApp.Device.CreateSpiBus(48000L), 
                             MeadowApp.Device.Pins.D02, MeadowApp.Device.Pins.D01, MeadowApp.Device.Pins.D00)
-    let mutable displayColor : Color = Color.DarkGreen.WithBrightness(25.0)
-
+    let mutable displayColor : Color = Color.White.WithBrightness(25.0)
     let mutable graphics = GraphicsLibrary(display)
-
     let updateDisplay newValue = 
         async {
             graphics.Clear(true)
@@ -48,18 +46,23 @@ type MeadowApp() =
             printfn "Ventilator OFF..." |> ignore
         }
 
-    let consumer = Ccs811.CreateObserver(fun result -> 
-        let mutable newValue = match result.New with | (co2, _) -> co2
+    let consumer = Ccs811.CreateObserver(fun result ->
+        let newValue = match result.New with | (co2, _) -> co2
         latestCO2Value <- newValue
-        printfn $"New CO2 value: {latestCO2Value}" |> ignore
+        let oldValue = match result.Old.Value with | (co2 , _) -> co2
+        if oldValue.HasValue then
+            previousCO2Value <- oldValue    
         displayColor <- match newValue.Value.PartsPerMillion with
                         | i when i >= 2000.0 -> Color.DarkRed
                         | i when i >= 1000.0 && i < 2000.0 -> Color.DarkOrange
                         | i when i >= 650.0 && i < 1000.0 -> Color.BurlyWood
                         | _ -> Color.DeepSkyBlue
-        updateDisplay newValue |> Async.Start |> ignore
+        if previousCO2Value.Value.PartsPerMillion <> latestCO2Value.Value.PartsPerMillion then
+            updateDisplay newValue |> Async.Start |> ignore 
+            printfn $"New CO2 value: {latestCO2Value}" |> ignore
         if newValue.Value.PartsPerMillion > triggerThreshold.Value.PartsPerMillion && ventilationIsOn = false then 
-            do toggleRelay 3000 |> Async.Start |> ignore)
+            toggleRelay 3000 |> Async.Start |> ignore)
+
 
     do sensor.StartUpdating(TimeSpan.FromSeconds(2.0))
     let mutable s = sensor.Subscribe(consumer)
