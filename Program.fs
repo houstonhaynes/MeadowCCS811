7027b949b0b8cb7d530d2aef93a8be4aa7370878
houstonhaynes <h3techsme@gmail.com>
2021-09-25 17:54:27 -0400

    recentered number display in box

--- Program.fs
+++ Program.fs
@@ -31,17 +31,17 @@ type MeadowApp() =
     let originy = displayheight / 2
 
     let mutable displayColor : Color = Color.White.WithBrightness(25.0)
-    let graphics = GraphicsLibrary(display)
+    let mutable graphics = GraphicsLibrary(display)
     let updateDisplay (firstcolor: Color) (secondcolor: Color) newValue = 
         async {
-            graphics.Clear(true)
+            graphics.Clear(false)
             graphics.CurrentFont <- Font12x16()
             graphics.DrawCircle(originx, originy, 120, firstcolor, true)
             graphics.DrawCircle(originx, originy, 105, Color.Black, true)
             graphics.DrawCircle(originx, originy, 100, secondcolor, true)
             graphics.DrawRoundedRectangle(32, 98, 175, 44, 8, Color.Black, true)
-            graphics.DrawText(120, 92, $"{newValue}", displayColor.WithBrightness(1.0), 
-                GraphicsLibrary.ScaleFactor.X4, GraphicsLibrary.TextAlignment.Center)
+            graphics.DrawText(120, 98, $"{newValue}", displayColor.WithBrightness(1.0), 
+                GraphicsLibrary.ScaleFactor.X3, GraphicsLibrary.TextAlignment.Center)
             graphics.Show()
         }
 
@@ -75,15 +75,14 @@ type MeadowApp() =
                         | i when i >= 650.0 && i < 1000.0 -> Color.BurlyWood
                         | _ -> Color.DeepSkyBlue
         onboardLEDColor <- match newValue.Value.PartsPerMillion with
-                            | i when i >= 2000.0 -> Color.DarkRed
-                            | i when i >= 1000.0 && i < 2000.0 -> Color.DarkOrange
-                            | i when i >= 650.0 && i < 1000.0 -> Color.BurlyWood
-                            | _ -> Color.DeepSkyBlue
+                            | i when i >= 2000.0 -> Color.Orange
+                            | i when i >= 750.0 && i < 2000.0 -> Color.Yellow
+                            | _ -> Color.White
         if previousCO2Value.Value.PartsPerMillion <> latestCO2Value.Value.PartsPerMillion then
-            updateDisplay Color.Orange Color.Blue newValue |> Async.Start |> ignore 
+            updateDisplay Color.Orange Color.Blue newValue |> Async.StartAsTask |> ignore 
             printfn $"New CO2 value: {newValue}" |> ignore
         if newValue.Value.PartsPerMillion > ventThreshold.Value.PartsPerMillion && not ventilationIsOn then 
-            toggleVent 3000 |> Async.Start |> ignore)
+            toggleVent 3000 |> Async.StartAsTask |> ignore)
 
     do sensor.StartUpdating(TimeSpan.FromSeconds(2.0))
     let mutable s = sensor.Subscribe(consumer)
